1592bbb1c4015f9dd7d8f90e1ea466b1308440a6
russmack <russ_mack@hotmail.com>
2015-03-16 19:40:01 +0000

    Added comments.

--- brainfugo.go
+++ brainfugo.go
@@ -26,44 +26,64 @@ func scanSrc(filename string) []byte {
 
 // eval examines each byte of the brainfuck source code and processes it accordingly.
 func eval(b []byte) {
+	// The interpretor's memory registers.
 	c := [30000]byte{}
+	// The register pointer, points to the current register.
 	p := 0
+	// Keep track of nested loops.
 	nestedLoops := 0
+	// Iterate over the source code byte instructions.
+	// i is instruction pointer, not to be confused with register pointer.
 	for i := 0; i < len(b); i++ {
 		switch string(b[i]) {
 		case ">":
+			// Move pointer to next register.
 			p++
 		case "<":
+			// Move pointer to previous register.
 			p--
 		case "+":
+			// Increment value in current register.
 			c[p]++
 		case "-":
+			// Decrement value in current register.
 			c[p]--
 		case ".":
+			// Output value in current register.
 			fmt.Print(string(c[p]))
 		case ",":
+			// Read input into current register.
 			reader := bufio.NewReader(os.Stdin)
 			input, _ := reader.ReadString('\n')
 			c[p] = input[0]
 		case "[":
+			// Start of a loop.
+			// If the current register value is zero,
+			// move the instruction pointer to the instruction following the matching close.
 			if c[p] != 0 {
+				// Continue to next source byte.
 			} else {
 				i++
-				lend := "]"[0]
-				for nestedLoops > 0 || b[i] != lend {
+				loopend := "]"[0]
+				for nestedLoops > 0 || b[i] != loopend {
 					if b[i] == 91 {
+						// Count start of a nested loop.
 						nestedLoops++
 					}
 					if b[i] == 93 {
+						// Count end of a nested loop.
 						nestedLoops--
 					}
 					i++
 				}
 			}
 		case "]":
+			// End of a loop.
+			// If the current register value is non-zero,
+			// then move instruction pointer to instruction following start of loop.
 			i--
-			lbegin := "["[0]
-			for nestedLoops > 0 || b[i] != lbegin {
+			loopbegin := "["[0]
+			for nestedLoops > 0 || b[i] != loopbegin {
 				if b[i] == 91 {
 					nestedLoops--
 				}
